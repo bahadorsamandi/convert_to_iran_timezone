5f527e3532002ac2d2130647403319168f3df615
baha2r <baha2r@BAHADOR>
2020-08-30 11:05:04 +0430

    Add project files.

---
+++ converttoirantimezone/classes/persiandatetimenow.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Globalization;
+    public static class persiandatetimenow
+    {
+        public static DateTime persiatime(this DateTime value)
+        {
+            TimeZoneInfo timeZoneInfo;
+            DateTime dateTime;
+            //Set the time zone information to iran  Standard Time 
+            timeZoneInfo = TimeZoneInfo.FindSystemTimeZoneById("Iran Standard Time");
+            //Get date and time in iran  Standard Time 
+            dateTime = TimeZoneInfo.ConvertTime(DateTime.Now, timeZoneInfo);
+            return dateTime;
+        }
+    
+    }
